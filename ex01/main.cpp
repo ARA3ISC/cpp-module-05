1e19edf9073845aa8f9d5a29655a145f4b0d8e02
ara3isc <m.aneddame01@gmail.com>
2023-10-11 19:32:44 +0100

    will begin the third form in ex02

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,22 +6,24 @@
 /*   By: maneddam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 17:43:22 by maneddam          #+#    #+#             */
-/*   Updated: 2023/10/11 19:26:27 by maneddam         ###   ########.fr       */
+/*   Updated: 2023/10/11 19:31:30 by maneddam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
 
 int main()
 {
 	try
 	{
-		ShrubberyCreationForm form1("home");
-		ShrubberyCreationForm form2 = form1;
+		RobotomyRequestForm form1("home");
+		RobotomyRequestForm form2 = form1;
 
 
+		// std::cout << "target : " << form2._target << std::endl;
 		std::cout << "tosign : " << form2.getGradeToSign() << std::endl;
 		std::cout << "toexec : " << form2.getGradeToExec() << std::endl;
 
